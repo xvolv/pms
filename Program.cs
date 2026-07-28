843b5cbe54d8d3c6545a951984bf9e50db5e6fea
Bekizod <bekizodcancer@gmail.com>
2026-07-28 15:40:29 +0300

    adding DevExpress UI

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddDevExpressBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IApiService, ApiService>();
 
