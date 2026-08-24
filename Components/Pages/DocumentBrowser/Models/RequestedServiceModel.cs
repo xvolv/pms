b77f60b9dd21979dfccadaaf6aa4ae3fab367220
Bekizod <bekizodcancer@gmail.com>
2026-08-24 16:07:46 +0300

    adding document-browser of tab Profile

---
+++ Components/Pages/DocumentBrowser/Models/RequestedServiceModel.cs
@@ -0,0 +1,8 @@
+namespace ERP.V7.WebPMS.Components.DocumentBrowser.Models
+{
+    public class RequestedServiceModel
+    {
+        public string Service { get; set; } = string.Empty;
+        public string TargetTime { get; set; } = string.Empty;
+    }
+}
