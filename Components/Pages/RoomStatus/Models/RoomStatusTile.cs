7d1fa3c39dfa2e7c86c8df7d6ac7f1f8804ec5ac
salassefa <salassefa@gmail.com>
2026-08-24 16:08:43 +0300

    room inventory and room status pages. Added new RoomStatusTile model and RoomStatus.razor component. Updated Program.cs to include DevExpress Server Side Blazor Report Viewer service.

---
+++ Components/Pages/RoomStatus/Models/RoomStatusTile.cs
@@ -0,0 +1,10 @@
+namespace ERP.V7.WebPMS.Components.Pages.RoomStatus.Models;
+
+public class RoomStatusTile
+{
+    public string Room { get; set; } = "";
+    public string RoomType { get; set; } = "";
+    public string Location { get; set; } = "";
+    public string HkStatus { get; set; } = "";
+    public string ReservationStatus { get; set; } = "";
+}
